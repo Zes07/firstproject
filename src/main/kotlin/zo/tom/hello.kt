8d1625a1727a4963c4896cc6ec3bfb482deef2f7
seanlee <sean0207lee15@gmail.com>
2019-08-21 16:25:47 +0800

    kotlin var

--- src/main/kotlin/zo/tom/hello.kt
+++ src/main/kotlin/zo/tom/hello.kt
@@ -2,8 +2,14 @@ package zo.tom
 
 fun main(args: Array<String>) {
     //println("hello kotlin")
-    val p = Human()
-    p.hello()
+    //val p = Human()
+    //p.hello()
+    var nake : String
+    nake = "kes"
+    println(nake)
+    nake = "jjj"
+    println(nake)
+
 }
 
 class Human{
